c43defa6e0a719bdba3891209516f87e5cc12c54
João Cesar <Joo Cesar@CESAR>
2018-02-28 21:52:58 -0300

    salvando entrregas

---
+++ ProjetosAulas/Aula1_2802/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1_2802
+{
+    class Pessoa
+    {
+        public int PessoaId { get; set; }
+        public string Nome { get; set; }
+
+
+        
+    }
+}
--- ProjetosAulas/Aula1_2802/Program.cs
+++ ProjetosAulas/Aula1_2802/Program.cs
@@ -1,15 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Aula1_2802
+
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Pessoa p = new Pessoa();
+
+            string nome = string.Empty;
+            Console.WriteLine("Digite seu nome");
+            p.Nome = Console.ReadLine();
+
+            Console.WriteLine("Olá " + p.Nome);
+            
+            Console.ReadKey();
+
+
+
+
         }
     }
 }
