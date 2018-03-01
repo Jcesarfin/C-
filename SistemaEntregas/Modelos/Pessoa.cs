e351939d27a32c0b1ffad08dd6dec5656326a45c
João Cesar <Joo Cesar@CESAR>
2018-02-28 22:03:46 -0300

    relacionamento

---
+++ SistemaEntregas/Modelos/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos
+{
+    class Pessoa
+    {
+        public int PessoaID { get; set; }
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+
+        //relacionamento com a classe endereço
+        public int EnderecoID { get; set; }
+        public Endereco _Endereco { get; set; }     
+
+
+    }
+}
